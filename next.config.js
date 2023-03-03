3ff1245c78d78907ec6df3cb6349ac7c31fe03cc
flpmcruz <flpmireles@gmail.com>
2023-03-02 21:51:59 -0500

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com']
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
